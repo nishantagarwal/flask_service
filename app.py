0103e95118e21335a60ea693af4eeedfb284407a
Nishant Agarwal <agarwal.nishant07@gmail.com>
2019-09-10 14:52:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 import os
-from flask import Flask, request, session
+from flask import Flask, session, redirect, url_for, escape, request
 
 app = Flask(__name__)
 
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.before_request
 def set_session():
     session["pod"] = os.environ.get("POD_NAME", "default")
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return "Pod Id: {}".format(session["pod"])
+    print("Pod Id: {}".format(session["pod"]))
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
 
-if __name__ == "__main__":
-    app.secret_key = "session_key"
-    app.run(host="0.0.0.0", port=80)
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
